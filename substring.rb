4a6f6f56be868938ab0573111ec81d80514bc722
Andrei Teodor <popr2801@gmail.com>
2025-02-24 20:26:10 +0200

    Added substrings to repository

---
+++ substring.rb
@@ -0,0 +1,14 @@
+def substrings(string,dict)
+  hash = Hash.new{0}
+  dict.each do |word|
+    if string.scan(/#{Regexp.escape(word)}/).length != 0
+            hash[word] = string.scan(/#{Regexp.escape(word)}/i).length
+    end
+  end
+  return hash
+end
+
+
+my_dict = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+puts substrings("Howdy partner, sit down! How's it going?",my_dict)
---
+++ test.rb
@@ -0,0 +1,2 @@
+string = "Howdy partner, sit down! How's it going?"
+puts string.scan(/i/).length
